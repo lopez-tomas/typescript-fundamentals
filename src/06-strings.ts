f859f79b32b0fcd78862c7670680dcb804a6416d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-31 16:53:11 -0300

    [FEAT]: string type.

--- src/06-strings.ts
+++ src/06-strings.ts
@@ -1,7 +1,24 @@
 (() => {
-  let producTitle = "My amazing product";
+  let productTitle = "My amazing product";
   // productTitle = null;
   // productTitle = () => {};
   // productTitle = 123;
   productTitle = "My amazing product CHANGED";
+  console.log(`productTitle: ${productTitle}`);
+
+  const productDescription = "I'm bla bla ba bla bl s asasas";
+  console.log(`productDescription: ${productDescription}`);
+
+  let productPrice = 100;
+  let isNew: boolean = false;
+
+  const summary = `
+    title: ${productTitle}
+    description: ${productDescription}
+    price: ${productPrice}
+    isNew: ${isNew}
+  `
+  console.log(summary);
+
+  //const myString: String = ''; // not use String type, use string instead
 })();
