8b4effc67af22928a5641da1d143f040904ea382
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-31 18:23:04 -0300

    [FEAT]: working with classic & arrow functions in TS. Optional parameters in functions.

---
+++ src/12-functions.ts
@@ -0,0 +1,42 @@
+(() => {
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+
+  function createProductToJson(
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size: Sizes
+  ) {
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
+  }
+
+  const product1 = createProductToJson('P1', new Date(), 12, 'XL');
+  console.log(product1);
+  console.log(product1.title);
+  console.log(product1.stock);
+
+  const createProductToJsonV2 = (
+    title: string,
+    createdAt: Date,
+    stock: number,
+    size?: Sizes
+  ) => {
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
+  }
+
+  const product2 = createProductToJsonV2('P1', new Date(), 12);
+  console.log(product2);
+  console.log(product2.title);
+  console.log(product2.stock);
+  console.log(product2.size);
+})();
